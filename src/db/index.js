8b431aa5a0b06a1897a47bb180a57e5c5b444ec9
waleed ijaz <waleedijaz9@gmail.comm>
2025-10-09 12:20:36 +0500

    kept db name dynamically

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants/index.js";
 
 const connectDB = async () => {
-
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
     );
     console.log(
-      `MongoDB connected successfully to host: ${connectionInstance.connection.host}`
+      `✅ Connected to DB: ${process.env.DB_NAME} on host ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log(`Error connecting DB : ${error}`);
+    console.log(`❌ Error connecting DB: ${error}`);
   }
 };
 
